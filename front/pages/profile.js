e5f2f7234b09a4da4ae2c5fb56623f16c6a48768
billowycloud <bjkim0228@naver.com>
2020-11-25 16:30:28 +0900

    set next js

---
+++ front/pages/profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+
+const profile = () => {
+  return (
+    <AppLayout>
+      <div>프로필 페이지</div>
+    </AppLayout>
+  );
+};
+
+export default profile;
---
+++ front/pages/signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+
+const signup = () => {
+  return (
+    <AppLayout>
+      <div>회원가입 페이지</div>
+    </AppLayout>
+  );
+};
+
+export default signup;
